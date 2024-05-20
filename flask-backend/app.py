5812226e849988fadba77e1e3ec18eeb5d0a399c
chrislom12 <christiaanlombard12@gmail.com>
2024-05-20 15:46:23 +0200

    ✨ Add flask python init

---
+++ flask-backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, jsonify
+from flask.wrappers import Response
+import git  
+
+
+app = Flask(__name__)
+
+# Route for the GitHub webhook
+
+
+""" @app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200 """
+
+
+@app.route('/')
+def index():
+    return "Hello World"
