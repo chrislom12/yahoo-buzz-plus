2f5437dfe2a7253f0fda75937805f86cdbd60c11
chrislom12 <christiaanlombard12@gmail.com>
2024-05-22 12:26:54 +0200

    👷 Fix tests and CI

--- flask-backend/test_app.py
+++ flask-backend/test_app.py
@@ -1,19 +1,17 @@
-import unittest
+import pytest
 from flask import Flask
 from flask.testing import FlaskClient
-import app  # Assuming your Flask app code is in a file named app.py
+from app import app  # Assuming your Flask app code is in a file named app.py
 
-class FlaskAppTestCase(unittest.TestCase):
-    def setUp(self):
-        # Set up the Flask test client
-        app.app.config['TESTING'] = True
-        self.client: FlaskClient = app.app.test_client()
+@pytest.fixture
+def client():
+    # Set up the Flask test client
+    app.config['TESTING'] = True
+    client = app.test_client()
+    return client
 
-    def test_hello_world(self):
-        # Test the / route
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Welcome to my API')
-
-if __name__ == '__main__':
-    unittest.main()
+def test_hello_world(client):
+    # Test the / route
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Welcome to my API'
